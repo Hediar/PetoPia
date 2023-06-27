170f931e17e2292b2e94034805eb7cc6676976d4
Seryeong Lee <srlimvp@gmail.com>
2023-06-27 16:53:35 +0900

    데이터 형식 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import LoginPage from './pages/LoginPage';
 import Main from './pages/Main';
+import SignupPage from './pages/SignupPage';
 import './reset.css';
 import { styled } from 'styled-components';
 
 function App() {
   return (
     <div>
-      <Main />
+      {/* <Main /> */}
       {/* <MainWrapper>TEST</MainWrapper> */}
+      <LoginPage />
+      {/* <SignupPage /> */}
     </div>
   );
 }
