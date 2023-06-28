5392ec6923042957ba9bef44d1b95b7ba436f146
Seryeong Lee <srlimvp@gmail.com>
2023-06-29 00:37:44 +0900

    마이페이지 생성, 헤더 컴포넌트 분리

---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Footer from '../components/Frame/Footer';
+
+import { MainWrapper } from '../stylecomponents/Wrapper';
+import Headernav from '../components/Frame/Headernav';
+import { styled } from 'styled-components';
+
+function MyPage() {
+  return (
+    <>
+      <Headernav />
+      <MainWrapper>
+        <Mypagetitle>
+          <h1>My Page</h1>
+          <Updateprofilebtn>프로필 수정하기</Updateprofilebtn>
+        </Mypagetitle>
+        <h2>내가 작성한 게시글</h2>
+        <Section></Section>
+      </MainWrapper>
+      <Footer />
+    </>
+  );
+}
+
+export default MyPage;
+
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin: 0 auto;
+  margin-top: 40px;
+  font-weight: bold;
+`;
+
+const Mypagetitle = styled.div`
+  display: flex;
+  margin: 10px;
+  padding: 0px 20px;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Updateprofilebtn = styled.button`
+  background-color: #eb9307;
+  color: white;
+  font-weight: 600;
+  font-size: 0.9rem;
+  border-radius: 14px;
+  border: none;
+  padding: 10px;
+  margin: 3px;
+  height: 40px;
+  &:hover {
+    cursor: pointer;
+    background-color: #ff8f05;
+    color: black;
+  }
+`;
