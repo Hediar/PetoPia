001b88fea2db3757d275c33dedcf4881553eef7e
Seryeong Lee <srlimvp@gmail.com>
2023-07-01 17:20:20 +0900

    update 기능 작업 전 백업

--- src/pages/DetailFeedPage.jsx
+++ src/pages/DetailFeedPage.jsx
@@ -7,11 +7,17 @@ import Headernav from '../components/Frame/Headernav';
 import { useDispatch, useSelector } from 'react-redux';
 import { MainWrapper } from '../stylecomponents/Wrapper';
 import { deleteFids } from '../redux/modules/fids';
+import { commonButton } from '../stylecomponents/Button';
 
 const DetailFeedPage = () => {
   const [curuser, setCuruser] = useState('');
   const [fid, setFid] = useState('');
   const [checkUser, setcheckUser] = useState(false);
+  const [updateState, setupdateState] = useState(false);
+
+  const [newupdateTitle, setNewUpdateTitle] = useState('');
+  const [newupdateContent, setNewUpdateContent] = useState('');
+
   const user = auth.currentUser;
 
   const dispatch = useDispatch();
@@ -25,10 +31,9 @@ const DetailFeedPage = () => {
     })
   )[0];
 
-  const deleteFid = () => {
-    console.log('delete', uid);
-    console.log('?', fid);
+  const updateFid = () => {};
 
+  const deleteFid = () => {
     navigate('/');
     alert('삭제되었습니다!');
     dispatch(deleteFids(uid));
@@ -47,22 +52,33 @@ const DetailFeedPage = () => {
   return (
     <>
       <Headernav />
+
       <MainWrapper>
-        <h1>{`${fid.title}`}</h1>
-        <div>
-          <h2>{`${fid.about}`}</h2>
-          <p>작성자 {`${fid.createdBy}`}</p>
-        </div>
-        {checkUser && (
-          <div>
-            <button>수정</button>
-            <button onClick={deleteFid}>삭제</button>
-          </div>
+        {!updateState && (
+          <>
+            <h1>{`${fid.title}`}</h1>
+            <div>
+              <h2>{`${fid.about}`}</h2>
+              <p>작성자: {`${fid.createdBy}`}</p>
+            </div>
+            {checkUser && (
+              <div>
+                <Button onClick={updateFid}>수정</Button>
+                <Button onClick={deleteFid}>삭제</Button>
+              </div>
+            )}
+            <div>
+              <img src={`${fid.imgURL}`} alt="fid-img"></img>
+              <p>{`${fid.contents}`}</p>
+            </div>
+          </>
         )}
-        <div>
-          <img src={`${fid.imgURL}`} alt="fid-img"></img>
-          <p>{`${fid.contents}`}</p>
-        </div>
+        {!updateState && (
+          <>
+            <UpdateTextInput />
+            <UpdateContentTextArea />
+          </>
+        )}
       </MainWrapper>
       <Footer />
     </>
@@ -71,24 +87,26 @@ const DetailFeedPage = () => {
 
 export default DetailFeedPage;
 
-const Form = styled.form`
-  margin-bottom: 1rem;
+const Button = styled(commonButton)``;
+
+const UpdateTextInput = styled.input`
+  width: 360px;
+  height: 30px;
+  border: 4px solid #eb9307;
+  border-radius: 16px;
+  margin-top: 10px;
+  font-size: 20px;
+  padding: 10px;
+  box-shadow: 10px 5px 20px gray;
 `;
 
-const ContentWrapper = styled.div`
-  margin-bottom: 1rem;
-`;
-
-const Image = styled.img`
-  width: 50%;
-  height: auto;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-`;
-
-const Input = styled.input`
-  width: 50;
-  padding: 0.5rem;
-  border: 1px solid #ccc;
-  border-radius: 4px;
+const UpdateContentTextArea = styled.textarea`
+  width: 730px;
+  height: 200px;
+  border: 4px solid #eb9307;
+  border-radius: 20px;
+  margin-top: 10px;
+  font-size: 20px;
+  padding: 10px;
+  box-shadow: 10px 5px 20px gray;
 `;
