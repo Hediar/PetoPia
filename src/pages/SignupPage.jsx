9ac09729a78238671c5efd70b9d298bc96dc7abe
Seryeong Lee <srlimvp@gmail.com>
2023-07-03 00:08:20 +0900

    회원가입 시 로그인되는 문제 해결

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -47,6 +47,7 @@ function SignupPage() {
           displayName: displayName
         })
           .then(() => {
+            sessionStorage.clear();
             alert('회원가입이 완료되었습니다.');
             navigate('/login');
           })
